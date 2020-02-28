9ddddd0537b50f1f65dd1aa93e169cef5ba3f681
Peter Anderson <saringer.anderson@gmail.com>
2020-02-28 12:42:45 +0000

    Project setup

---
+++ public/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Apt 206</title>
+    <link href='./css/theme.css' rel='stylesheet'>
+</head>
+
+<body>
+
+<div class="grey-bar grey-bar__top"></div>
+<div class="grey-bar grey-bar__left"></div>
+<div class="grey-bar grey-bar__right"></div>
+<ul>
+    <li>Work</li>
+    <li>About</li>
+    <li>Contact</li>
+</ul>
+
+<section>
+    Work
+</section>
+<section>
+    About
+</section>
+<section>
+    Contact
+</section>
+
+</body>
+</html>
