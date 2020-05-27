da1a83807e1793f5c57d609faf99b2c4ba646411
Peter Anderson <saringer.anderson@gmail.com>
2020-05-27 14:05:53 +0100

    Dark layout of site

--- public/index.php
+++ public/index.php
@@ -1,114 +1,210 @@
-<!doctype html>
-
+<!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="utf-8">
-
-    <title>Apt 206</title>
-    <meta name="description" content="The HTML5 Herald">
-    <meta name="author" content="SitePoint">
-
-    <link href='./css/theme.css' rel='stylesheet'>
-
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+    <meta name="description" content=""/>
+    <meta name="author" content=""/>
+    <title>Apt206.net | Peter Anderson</title>
+    <link rel="icon" type="image/x-icon" href="img/apt206.png"/>
+    <!-- Font Awesome -->
+    <script src="https://use.fontawesome.com/releases/v5.12.1/js/all.js" crossorigin="anonymous"></script>
+    <!-- Google fonts-->
+    <link href="https://fonts.googleapis.com/css?family=Varela+Round" rel="stylesheet"/>
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,400,700"
+          rel="stylesheet"/>
+    <!-- CSS )-->
+    <link href="css/bootstrap.min.css" rel="stylesheet"/>
+    <link href="css/theme.css" rel="stylesheet"/>
 </head>
 
-<body>
-<header>
-    <menu><ul>
-            <li><a href="#work">Work</a></li>
-            <li><a href="#about">About</a></li>
-            <li><a href="#contact">Contact</a></li>
-        </ul></menu>
-    <div class="logo">
-        <a href=""><img src="img/apt206_logo.jpg" alt="Apt 206"></a>
+<body id="page-top">
+
+<!-- Navigation-->
+<nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+    <div class="container">
+        <a class="navbar-brand js-scroll-trigger" href="#page-top"><img src="/img/apt206-logo.png" alt=""/></a>
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse"
+                data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false"
+                aria-label="Toggle navigation"><i class="fas fa-bars"></i></button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#about">About</a></li>
+                <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#work">Work</a></li>
+                <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#contact">Contact</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+<!-- Masthead-->
+<header class="masthead">
+    <div class="container d-flex h-100 align-items-center">
+        <div class="mx-auto">
+            <h1 class="mx-auto">Peter Anderson</h1>
+            <p class="mx-auto">Web design and development</p>
+        </div>
     </div>
 </header>
 
-    <div class="grey-bar grey-bar__top"></div>
-    <div class="grey-bar grey-bar__left"></div>
-    <div class="grey-bar grey-bar__right"></div>
-<div class="hero-section">
-    <div class="hero-link-section">
-        <a href="#work">
-            <div class="hero-link">
-                <h1 data-text="Work" class="fade-in one">Work</h1>
-            </div>
-        </a>
-        <a href="#about">
-            <div class="hero-link">
-
-                <h1 data-text="about" class="fade-in two">About</h1>
-            </div>
-        </a>
-        <a href="#contact">
-            <div class="hero-link">
-                <a href="#contact">
-                <h1 data-text="contact" class="fade-in three">Contact</h1>
-                </a>
-            </div>
-        </a>
-
-    </div>
-</div>
-
-<section id="work">
-    <h2>Work</h2>
-</section>
-
+<!-- About-->
 <section id="about">
-    <h2>About</h2>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-10 mx-auto">
+                <h2>About</h2>
+                <div class="row align-items-center no-gutters">
+                    <div class="col-xl-6 col-lg-7">
+                        <img class="img-fluid" src="/img/Cedarburg-bw.jpg" alt=""/>
+                    </div>
+                    <div class="col-xl-6 col-lg-5">
+                        <p>Raised in a small city on Wisconsin, currently reside in Chesham.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </section>
 
+<!-- Work-->
+<section id="work">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-xs-12">
+                <h2>Work</h2>
+            </div>
+        </div>
+        <!-- Work One Row-->
+        <div class="row justify-content-center no-gutters mb-5 mb-lg-0">
+            <div class="col-lg-6"><img src="/img/livingroom-50.jpg" alt=""/></div>
+            <div class="col-lg-6">
+                <div class="bg-black text-center h-100 work">
+                    <div class="d-flex h-100">
+                        <div class="work-text w-100 my-auto text-center text-lg-left">
+                            <h4>Design Portfolio</h4>
+                            <p>Building websites for a client can have its challenges, building your own company site is
+                                another story.</p>
+                            <hr class="d-none d-lg-block mb-0 ml-0"/>
+                            <a href="https://www.design-portfolio.co.uk/" target="_blank">design-portfolio.co.uk</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Work Two Row-->
+        <div class="row justify-content-center no-gutters mb-5 mb-lg-0">
+            <div class="col-lg-6"><img src="/img/bathroom-50.jpg" alt=""/></div>
+            <div class="col-lg-6 order-lg-first">
+                <div class="bg-black text-center h-100 work">
+                    <div class="d-flex h-100">
+                        <div class="work-text w-100 my-auto text-lg-left">
+                            <h4>Casa Alfarroba</h4>
+                            <p>Simple single page site rebuilt to highlight a family holiday home in the Algarve.</p>
+                            <hr class="d-none d-lg-block mb-0 mr-0"/>
+                            <a href="http://casaalfarroba.com/" target="_blank">casaalfarroba.com</a>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Work Three Row-->
+        <div class="row justify-content-center no-gutters mb-5 mb-lg-0">
+            <div class="col-lg-6"><img src="/img/office2-50.jpg" alt=""/></div>
+            <div class="col-lg-6">
+                <div class="bg-black text-center h-100 work">
+                    <div class="d-flex h-100">
+                        <div class="work-text w-100 my-auto text-center text-lg-left">
+                            <h4>Total clarity</h4>
+                            <p>A Design Portfolio project, the client requested a flat html site to highlight the
+                                positives that diamond producers are working to bring into the world</p>
+                            <hr class="d-none d-lg-block mb-0 mr-0"/>
+                            <a href="https://totalclarity.naturaldiamonds.com/" target="_blank">totalclarity.naturaldiamonds.com</a>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Work Four Row-->
+        <div class="row justify-content-center no-gutters mb-5 mb-lg-0">
+            <div class="col-lg-6"><img src="/img/kitchen-50.jpg" alt=""/></div>
+            <div class="col-lg-6 order-lg-first">
+                <div class="bg-black text-center h-100 work">
+                    <div class="d-flex h-100">
+                        <div class="work-text w-100 my-auto text-lg-left">
+                            <h4>The Pebble Group</h4>
+                            <p>A Design Portfolio project built using the concrete 5 framework. This investor relations
+                                website was a quick turn around with different factors that needed to be met to ensure
+                                maximum protection of data was kept while information was released.</p>
+                            <hr class="d-none d-lg-block mb-0 ml-0"/>
+                            <a href="https://www.thepebblegroup.com/" target="_blank">thepebblegroup.com</a>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- Contact-->
 <section id="contact">
-    <h3>Contact</h3>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-xs-12">
+                <h2>Contact</h2>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-xs-12 col-md-4">
+                <div class="card py-4">
+                    <div class="card-body text-center">
+                        <h3 class="text-uppercase m-0">Email</h3>
+                        <hr/>
+                        <a href="mailto:saringer.anderson@gmail.com">
+                            <i class="fas fa-envelope mb-2"></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xs-12 col-md-4">
+                <div class="card py-4">
+                    <div class="card-body text-center">
+                        <h3 class="text-uppercase m-0">Linkedin</h3>
+                        <hr/>
+                        <a href="https://www.linkedin.com/in/peter-anderson-0458a8135/" target="_blank">
+                            <i class="fab fa-linkedin-in mb-2"></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xs-12 col-md-4">
+                <div class="card py-4">
+                    <div class="card-body text-center">
+                        <h3 class="text-uppercase m-0">Github</h3>
+                        <hr/>
+                        <a href="https://github.com/Saringer82" target="_blank">
+                            <i class="fab fa-github mb-2"></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </section>
 
-<footer></footer>
-
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-
-<script>
-    //Script for navigation scroll
-
-    $(document).ready(function () {
-        // Add smooth scrolling to all links
-        $("a").on('click', function (event) {
-
-            // Make sure this.hash has a value before overriding default behavior
-            if (this.hash !== "") {
-                // Prevent default anchor click behavior
-                event.preventDefault();
-
-                // Store hash
-                var hash = this.hash;
-
-                // Using jQuery's animate() method to add smooth page scroll
-                // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-                $('html, body').animate({
-                    scrollTop: $(hash).offset().top
-                }, 800, function () {
-
-                    // Add hash (#) to URL when done scrolling (default click behavior)
-                    window.location.hash = hash;
-                });
-            } // End if
-        });
-
-        //Show/hide main menu on scroll
-        $(window).scroll(function(){
-
-            if ($(window).scrollTop() > $(window).height()) {
-                $('menu').fadeIn(100);
-            }
-
-            else {
-                $('menu').fadeOut(100);
-            }
-        });
-    });
-</script>
-
+<!-- Footer-->
+<footer class="footer">
+    <div class="container">Copyright © Apt206 2020</div>
+</footer>
+<!-- Bootstrap core JS-->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"></script>
+<!-- Third party plugin JS-->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
+<!-- Core theme JS-->
+<script src="js/scripts.js"></script>
 </body>
 </html>
